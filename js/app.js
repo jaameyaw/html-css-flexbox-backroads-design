8ab7e502f1a5c0294a5c8234fc5507bb216faa14
jaameyaw <johnsonameyaw22@gmail.com>
2025-05-30 05:09:52 +0000

    Add script to hide loading screen after DOM loads

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 'strict mode';
+// ********** loading screen ************
+//Hides the loading screen immediately after the DOM has fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    const loader = document.getElementById("loader");
+    if (loader) {
+      loader.classList.remove("show");
+    }
+  }, 1);
+});
+
 // ********** nav toggle ************
 // select button and links
 const navBtn = document.getElementById("nav-toggle");
